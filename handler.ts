1e81a91c7a2d8bb597601791d5cee6f712cf220c
Sebastian Marines <sebastian0marines@gmail.com>
2022-09-21 14:59:04 -0500

    Save file to disk

--- handler.ts
+++ handler.ts
@@ -1,4 +1,7 @@
 import * as aws from "@pulumi/aws";
+import * as fs from 'fs';
+import Jimp from "jimp";
+import { streamToBuffer } from "@jorgeferrero/stream-to-buffer";
 
 
 export const onObjectCreatedHandler = async (event: aws.s3.BucketEvent) => {
@@ -19,8 +22,9 @@ export const onObjectCreatedHandler = async (event: aws.s3.BucketEvent) => {
         Bucket: event.Records[0].s3.bucket.name,
         Key: objectName,
     };
-    const object = client.getObject(params).createReadStream();
-    const objectType = await fileTypeFromStream(object)
+    const object = client.getObject(params);
+    const objectStream = object.createReadStream();
+    const objectType = await fileTypeFromStream(objectStream)
 
     if (!objectType) {
         throw new Error("Could not determine file type");
@@ -31,5 +35,35 @@ export const onObjectCreatedHandler = async (event: aws.s3.BucketEvent) => {
         return;
     }
 
+    const buffer = await streamToBuffer(objectStream);
+
+    fs.writeFile(objectName, buffer, (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+
+    const image = await Jimp.read(objectName);
+    // await image.writeAsync(objectName + ".png");
+
+    // Save object to disk
+
+
+    // console.log(`Object is an image: ${objectName}`);
+    // const objectEncoded = object.read().toString("base64");
+    // // Convert to a buffer
+    // console.log(`Object encoded: ${objectEncoded}`);
+    // const buffer = Buffer.from(objectEncoded, "base64");
+    // console.log(`Buffer: ${buffer}`);
+    // const image = await Jimp.read(buffer);
+    const processed = await image.getBufferAsync(Jimp.MIME_PNG);
+
+    const params_put = {
+        Bucket: event.Records[0].s3.bucket.name,
+        Key: objectName,
+        Body: processed,
+        ContentType: "image/png",
+    };
+    await client.putObject(params_put).promise();
 
 }
